c7d6836861453f277e92e17939e1ac1b0bfda2aa
pixelwavestudio2126-sudo <pixelwavestudio2126@gmail.com>
2026-08-07 13:31:19 +0530

    Refactor openImage and closeImage functions

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 console.log("Pixel Wave Studio Website Loaded!");
-function openImage(src){
+
+function openImage(src) {
     document.getElementById("lightbox").style.display = "flex";
     document.getElementById("lightbox-img").src = src;
 }
 
-function closeImage(){
+function closeImage() {
     document.getElementById("lightbox").style.display = "none";
 }
+
 const counters = document.querySelectorAll(".count");
 
 counters.forEach(counter => {
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText.replace(/\D/g, "");
-    const increment = Math.ceil(target / 100);
 
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCounter, 20);
-    } else {
-      if (target >= 1000) {
-        counter.innerText = (target / 1000) + "K+";
-      } else if (target === 100) {
-        counter.innerText = "100%";
-      } else {
-        counter.innerText = target;
-      }
-    }
-  updateCounter();
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText.replace(/\D/g, "");
+        const increment = Math.ceil(target / 100);
+
+        if (count < target) {
+
+            counter.innerText = count + increment;
+            setTimeout(updateCounter, 20);
+
+        } else {
+
+            if (target >= 1000) {
+                counter.innerText = (target / 1000) + "K+";
+            } else if (target === 100) {
+                counter.innerText = "100%";
+            } else {
+                counter.innerText = target;
+            }
+
+        }
+    };
+
+    updateCounter();
+
 });

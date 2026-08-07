ee88da6d59007bf4c3f5a4c01a9c0d38947a751f
pixelwavestudio2126-sudo <pixelwavestudio2126@gmail.com>
2026-08-07 12:34:32 +0530

    Add counter animation functionality

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function openImage(src){
 function closeImage(){
     document.getElementById("lightbox").style.display = "none";
 }
+const counters = document.querySelectorAll(".count");
+
+counters.forEach(counter => {
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const increment = target / 100;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
